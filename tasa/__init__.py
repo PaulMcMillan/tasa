6de38e63414017e2745e2353fd58f345834e097f
Paul McMillan <paul.mcmillan@nebula.com>
2013-06-07 19:46:48 -0400

    Handle versioning when tasa is not installed

--- tasa/__init__.py
+++ tasa/__init__.py
@@ -1,3 +1,7 @@
 import pkg_resources
 
-__version__ = pkg_resources.get_distribution("tasa").version
+try:
+    __version__ = pkg_resources.get_distribution("tasa").version
+except pkg_resources.DistributionNotFound:
+    # There's probably a better way to handle this
+    __version__ = 'UNKNOWN'
