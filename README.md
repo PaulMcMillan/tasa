c7238f4ecb8cc44b33f91113ace3747f1fd4b553
Paul McMillan <paul.mcmillan@nebula.com>
2013-06-07 17:12:25 -0400

    Update License to BSD, add setup.py

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ the box.
 Getting Started
 ---------------
 
+Tasa requires a version of Python 2.7. If you're using an older
+version, now is an excellent time to upgrade. If you're running Python
+3, compatibility patches are welcome.
+
 Make sure you have redis and the python redis bindings installed. If
 you're using a remote redis instance, you can set the `REDIS_ADDRESS`
 environment variable.
---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name="tasa",
+    version='0.1dev1',
+    description="A simple job queue framework using redis.",
+    long_description=open("README.md").read(),
+    url='https://github.com/PaulMcMillan/tasa',
+    license="Simplified BSD",
+    author="Paul McMillan",
+    author_email="paul@mcmillan.ws",
+
+    install_requires=[
+        "redis",
+        ],
+    extras_require={
+        "tests": [
+            "pep8",
+            "pylint",
+            "pytest",
+            "pytest-cov",
+            ],
+        },
+    tests_require=[
+        "pep8",
+        "pytest",
+        "pytest-cov",
+        ],
+
+    packages=find_packages(exclude=['tests', 'examples']),
+
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ])
+
+
+
